ab3995096efbab687a0cbeac9e725d2b20bfa41c
Vajni <vajnork.zsolt@gmail.com>
2017-04-26 10:02:00 +0200

    Refactored the followings: Added overload method for DataSelection class Refactored the Login servlet to use DB

--- src/DataSelection.java
+++ src/DataSelection.java
@@ -43,4 +43,45 @@ public class DataSelection {
         return null;
     }
 
+    public String selectDataByHeader(String selectValue, String table, String header, String value) throws SQLException {
+
+        Connection myConn = null;
+        Statement myStmt = null;
+        ResultSet myRs = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            myConn = DriverManager.getConnection(DataBaseEnums.URL.dbData, DataBaseEnums.USER.dbData ,
+                    DataBaseEnums.PASSWORD.dbData);
+
+            myStmt = myConn.createStatement();
+
+            myRs = myStmt.executeQuery("SELECT "  + selectValue + " FROM " + table + " WHERE " + header + "=" + "\'" + value + "\'");
+
+            String returnString = "";
+
+            while (myRs.next()) {
+                returnString += myRs.getString(selectValue);
+            }
+            return returnString;
+        }
+        catch (Exception exc) {
+            exc.printStackTrace();
+        }
+        finally {
+            if (myRs != null) {
+                myRs.close();
+            }
+
+            if (myStmt != null) {
+                myStmt.close();
+            }
+
+            if (myConn != null) {
+                myConn.close();
+            }
+        }
+        return null;
+    }
+
 }
